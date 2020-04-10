8c53fa6a24143175e622bd0838eafcbac5300bc8
cthscr <cthscr@gmail.com>
2020-04-10 13:46:48 +0300

    Fix door of Superior Temple of Baa checks for Repair (#574).

---
+++ Scripts/Maps/6t7.lua
@@ -0,0 +1,11 @@
+
+Game.MapEvtLines:RemoveEvent(1)
+evt.hint[1] = evt.str[1]  -- "Door"
+evt.map[1] = function()
+	evt.ForPlayer("Current")
+	if evt.CheckSkill{const.Skills.Perception, Mastery = const.Novice, Level = 8} then
+		evt.SetDoorState{Id = 1, State = 1}
+	else
+		evt.DamagePlayer{Player = "Current", DamageType = const.Damage.Fire, Damage = 50}
+	end
+end
